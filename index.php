f6238bc924b35cc41646d69d21b7484f762a9cd6
Jullyane Hawkins <jullyanehawkins@gmail.com>
2018-05-03 08:43:37 -0600

    created delete method

--- index.php
+++ index.php
@@ -16,3 +16,21 @@ public function insert(\PDO $pdo) : void {
 	$parameters = ["senatorId" => $this->senatorId->getBytes(), "senatorName" => $this->senatorName->getBytes(), "senatorNumLives" => $this->senatorNumLives];
 	$statement->execute($parameters);
 }
+
+/**
+ * deletes this Senator from mySQL
+ *
+ * @param \PDO $pdo PDO connection object
+ * @throws \PDOException when mySQL related errors occur
+ * @throws \TypeError if $pdo is not a PDO connection object
+ **/
+public function delete(\PDO $pdo) : void {
+
+	// create query template
+	$query = "DELETE FROM senator WHERE senatorId = :senatorId";
+	$statement = $pdo->prepare($query);
+
+	// bind the member variables to the place holder in the template
+	$parameters = ["senatorId" => $this->senatorId->getBytes()];
+	$statement->execute($parameters);
+}
